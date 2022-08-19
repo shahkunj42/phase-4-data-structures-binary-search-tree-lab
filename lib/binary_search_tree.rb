4686998cb6df3d0488e8ef514f857f8fae930bb5
Kunj <shahkunj42@gmail.com>
2022-08-19 18:00:42 -0400

    commit tree 63fef86dc12b47b428bee167918b5179cafe332d

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -7,12 +7,50 @@ class BinarySearchTree
     @root = root
   end
 
-  def search(value)
-    # your code here
+  def insert(value)
+    new_node = Node.new(value)
+    if @root.nil?
+      @root = new_node
+      return @root
+    end
+
+    current = @root
+
+    while current
+      return nil if value == current.value
+
+      if value < current.value
+        if current.left.nil?
+          current.left = new_node
+          return current.left
+        end
+        current = current.left
+      else
+        if current.right.nil?
+          current.right = new_node
+          return current.right
+        end
+        current = current.right
+      end
+    end
   end
 
-  def insert(value)
-    # your code here
+  def search(value)
+    return nil if @root.nil?
+
+    current = @root
+    found = false
+
+    while current && !found
+      if value < current.value
+        current = current.left
+      elsif value > current.value
+        current = current.right
+      else
+        found = true
+      end
+    end
+    found ? current : nil
   end
 
 end 
